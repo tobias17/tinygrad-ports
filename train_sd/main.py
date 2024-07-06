2ae21eb98e32572e5e3bd2a9e121188f5d332a16
Tobias Fischer <tobiasfischer17@gmail.com>
2024-07-06 00:58:17 +0000

    training runs, turns into nans

--- train_sd/main.py
+++ train_sd/main.py
@@ -1,29 +1,30 @@
-from tinygrad import Tensor, dtypes # type: ignore
+from tinygrad import Tensor, dtypes, TinyJit, Device # type: ignore
 from tinygrad.nn.optim import AdamW # type: ignore
 
-from PIL import Image
 from typing import Dict, Tuple
+import numpy as np
 
-from tinygrad.nn.state import load_state_dict, torch_load # type: ignore
+from tinygrad.nn.state import load_state_dict, torch_load, get_parameters, get_state_dict # type: ignore
 
 from extra.models.unet import UNetModel # type: ignore
 from examples.sdv2 import params, FrozenOpenClipEmbedder, get_alphas_cumprod # type: ignore
 
 if __name__ == "__main__":
-  BS = 2
+  BS = 1
   TRAIN_DTYPE = dtypes.float16
-
-
-
 
   class WrapperModel:
     def __init__(self, cond_stage_config:Dict, **kwargs):
       self.cond_stage_model = FrozenOpenClipEmbedder(**cond_stage_config)
   wrapper = WrapperModel(**params)
   load_state_dict(wrapper, torch_load("/home/tiny/tinygrad/weights/512-base-ema.ckpt")["state_dict"])
+  # for w in get_state_dict(wrapper).values():
+  #   w.to(WRAPPER_DEVICE)
 
   model = UNetModel(**params["unet_config"])
-
+  for w in get_state_dict(model).values():
+    w.replace(w.cast(dtypes.float16)).realize()
+  optimizer = AdamW(get_parameters(model), lr=1.25e-7, b1=0.9, b2=0.999, eps=1e-08, weight_decay=0.01)
 
 
 
@@ -50,35 +51,38 @@ if __name__ == "__main__":
   dataset = dataset.shuffle(size=1000).decode().map(filter_fnx).batched(BS, partial=False, collation_fn=collate_fnx)
   dataloader = WebLoader(dataset, batch_size=None, shuffle=False, num_workers=4, persistent_workers=True)
 
-  import numpy as np
-  for i, entry in enumerate(dataloader):
-    break
-
-  print(entry.keys())
-  print(type(entry["moments"]))
-  print(entry["moments"].shape)
-
   def sample_moments(moments:Tensor) -> Tensor:
     mean, logvar = moments.chunk(2, dim=1)
     logvar = logvar.clip(-30.0, 20.0)
     std = Tensor.exp(logvar * 0.5)
     return mean + std * Tensor.rand(*mean.shape)
 
-  x = (sample_moments(entry["moments"]) * 0.18215).cast(TRAIN_DTYPE)
-  c = Tensor.cat(*[wrapper.cond_stage_model(t) for t in entry["txt"]], dim=0)
-  t = Tensor.randint(x.shape[0], low=0, high=1000)
-
-  def extract_into_tensor(a:Tensor, t:Tensor, x_shape:Tuple[int,...]) -> Tensor:
-    return a.gather(-1, t).reshape(t.shape[0], *((1,) * (len(x_shape) - 1)))
-
-  def train_step(x:Tensor, c:Tensor):
+  # @TinyJit
+  def train_step(x:Tensor, c:Tensor, t:Tensor) -> None:
+    Tensor.training = True
+    
     noise   = Tensor.randn(x.shape)
-    x_noisy = sqrt_alphas_cumprod         .gather(-1, t).reshape(x.shape[0], 1, 1, 1) * x \
-            + sqrt_on_minus_alphas_cumprod.gather(-1, t).reshape(x.shape[0], 1, 1, 1) * noise
+    x_noisy =   sqrt_alphas_cumprod         .gather(-1, t).reshape(x.shape[0], 1, 1, 1) * x \
+              + sqrt_on_minus_alphas_cumprod.gather(-1, t).reshape(x.shape[0], 1, 1, 1) * noise
     output  = model(x_noisy, t, c)
 
-    loss = (x - output).square().mean([1, 2, 3]).mean()
+    loss = (x.cast(dtypes.float32) - output.cast(dtypes.float32)).square().mean([1, 2, 3]).mean()
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
+    lv = loss.numpy()
+    print(lv)
 
+  def prep_for_jit(*inputs:Tensor) -> Tuple[Tensor,...]:
+    return tuple(i.cast(TRAIN_DTYPE).realize() for i in inputs)
 
+  for i, entry in enumerate(dataloader):
+    x = (sample_moments(entry["moments"]) * 0.18215)
+    c = Tensor.cat(*[wrapper.cond_stage_model(t) for t in entry["txt"]], dim=0)
+    t = Tensor.randint(x.shape[0], low=0, high=1000)
+    print(f"Running Step {i}")
+    train_step(*prep_for_jit(x, c, t))
+    if i >= 20:
+      break
 
