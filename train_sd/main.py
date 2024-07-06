f8cf097f5c8c5edc6a79c33cb979f3699e101d73
Tobias Fischer <tobiasfischer17@gmail.com>
2024-07-06 20:38:30 +0000

    cleanup

--- train_sd/main.py
+++ train_sd/main.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 
 from typing import Dict, Tuple
 import numpy as np
-import time, os
+import time, os, datetime
+import torch
 
 from tinygrad.nn.state import load_state_dict, torch_load, get_parameters, get_state_dict # type: ignore
 
@@ -23,6 +24,15 @@ from examples.sdv2 import params, FrozenOpenClipEmbedder, get_alphas_cumprod # t
 
 
 if __name__ == "__main__":
+  seed = 42
+  Tensor.manual_seed(seed)
+  torch.manual_seed(seed)
+
+  __OUTPUT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), "runs", datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
+  def get_output_root() -> str:
+    if not os.path.exists(__OUTPUT_ROOT):
+      os.mkdir(__OUTPUT_ROOT)
+    return __OUTPUT_ROOT
 
   TRAIN_DTYPE = dtypes.float16
 
@@ -30,6 +40,9 @@ if __name__ == "__main__":
   GPUS = [f'{Device.DEFAULT}:{i}' for i in [1,2,3,4,5]]
   DEVICE_BS = 2
   GLOBAL_BS = DEVICE_BS * len(GPUS)
+
+
+  # Model, Conditioner, Other Variables
 
   class WrapperModel:
     def __init__(self, cond_stage_config:Dict, **kwargs):
@@ -44,20 +57,15 @@ if __name__ == "__main__":
     w.replace(w.cast(dtypes.float16).shard(GPUS, axis=None)).realize()
   # optimizer = AdamW(get_parameters(model), lr=1.25e-7, b1=0.9, b2=0.999, weight_decay=0.01)
   # optimizer = AdamW(get_parameters(model), lr=1.25e-7, eps=1.0)
-  optimizer = SGD(get_parameters(model), lr=1.25e-7)
-
-
+  optimizer = SGD(get_parameters(model), lr=1.25e-7 *50)
 
   alphas_cumprod      = get_alphas_cumprod()
   alphas_cumprod_prev = Tensor([1.0]).cat(alphas_cumprod[:-1])
   sqrt_alphas_cumprod = alphas_cumprod.sqrt()
   sqrt_on_minus_alphas_cumprod = (1.0 - alphas_cumprod).sqrt()
 
-  alphas_cumprod              .realize()
-  alphas_cumprod_prev         .realize()
-  sqrt_alphas_cumprod         .realize()
-  sqrt_on_minus_alphas_cumprod.realize()
 
+  # Dataset
 
   def collate_fnx(batch):
     arr = np.zeros((len(batch),8,64,64), dtype=np.float32)
@@ -66,8 +74,7 @@ if __name__ == "__main__":
     return { "moments": Tensor(arr), "txt": [e["txt"] for e in batch] }
 
   from webdataset import WebDataset, WebLoader # type: ignore
-  # urls = "/home/tiny/tinygrad/datasets/laion-400m/webdataset-moments-filtered/{00000..00831}.tar"
-  urls = "/home/tiny/tinygrad/datasets/laion-400m/webdataset-moments-filtered/{00000..00010}.tar"
+  urls = "/home/tiny/tinygrad/datasets/laion-400m/webdataset-moments-filtered/{00000..00831}.tar"
   keep_only_keys = { "npy", "txt" }
   def filter_fnx(sample): return {k:v for k,v in sample.items() if k in keep_only_keys}
   dataset = WebDataset(urls=urls, resampled=True, cache_size=-1, cache_dir=None)
@@ -79,6 +86,9 @@ if __name__ == "__main__":
     logvar = logvar.clip(-30.0, 20.0)
     std = Tensor.exp(logvar * 0.5)
     return mean + std * Tensor.rand(*mean.shape)
+
+
+  # Train Funcs and Utils
 
   @TinyJit
   def train_step(x:Tensor, c:Tensor, x_noisy:Tensor, t_emb:Tensor) -> Tensor:
@@ -96,15 +106,19 @@ if __name__ == "__main__":
   def prep_for_jit(*inputs:Tensor) -> Tuple[Tensor,...]:
     return tuple(i.cast(TRAIN_DTYPE).shard(GPUS, axis=0).realize() for i in inputs)
 
-  SAVE_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), "runs"))
-  if not os.path.exists(SAVE_DIR):
-    os.mkdir(SAVE_DIR)
-
-  MAX_ITERATIONS = 5000
-  GRAPH_EVERY = 20
+  MAX_ITERATIONS = 50000
+  MEAN_EVERY = 10
+  GRAPH_EVERY = 100
   losses = []
+  saved_losses = []
+
+
+  # Main Train Loop
 
   for i, entry in enumerate(dataloader):
+    if i >= MAX_ITERATIONS:
+      break
+
     st = time.perf_counter()
 
     x = (sample_moments(entry["moments"]) * 0.18215)
@@ -122,9 +136,10 @@ if __name__ == "__main__":
 
     tqdm.write(f"{i:05d}: {(et-st)*1000.0:>6.2f} ms run, {loss:>2.5f} train loss")
 
+    if i > 0 and i % MEAN_EVERY == 0:
+      saved_losses.append(sum(losses) / len(losses))
+      losses = []
     if i > 0 and i % GRAPH_EVERY == 0:
       plt.clf()
-      plt.plot(np.arange(len(losses)), losses)
-      plt.savefig(os.path.join(SAVE_DIR, "loss"))
-    if i >= MAX_ITERATIONS:
-      break
+      plt.plot(np.arange(len(saved_losses))*MEAN_EVERY, saved_losses)
+      plt.savefig(os.path.join(get_output_root(), "loss"))
