6a480f64aabaea9e6a9babd38562a24bfaf46b02
Tobias Fischer <tobiasfischer17@gmail.com>
2024-07-11 23:10:24 +0000

    figured out issue with sampling

--- train_sd/ddim.py
+++ train_sd/ddim.py
@@ -23,7 +23,7 @@ class DdimSampler:
 
     tms_tensor  = Tensor(ddim_timesteps)
     alphas      = alphas_cumprod[tms_tensor]
-    alphas_prev = alphas_cumprod_prev[tms_tensor]
+    alphas_prev = Tensor.cat(alphas_cumprod[:1], alphas_cumprod[tms_tensor[:-1]])
     sigmas      = Tensor.zeros_like(alphas)
 
     x_t = Tensor.randn(batch_size, 4, 64, 64)
@@ -34,10 +34,8 @@ class DdimSampler:
       return model(x,t,c).realize()
 
     for i, step in enumerate(tqdm(time_range)):
-      if i < 9:
-        continue
-      if i == 9:
-        x_t = Tensor(np.load(f"/home/tiny/weights_cache/ddim/img_step_{i}.npy"))
+      x_t = Tensor(np.load(f"/home/tiny/weights_cache/ddim/img_step_{i}.npy"))
+      print(f"ITER {i}")
 
       index = num_steps - i - 1
       tms = Tensor.full((batch_size,), int(step))
@@ -46,35 +44,36 @@ class DdimSampler:
       def fp16r(z):
         return z.cast(dtypes.float16).realize()
 
-      a,b = Tensor.cat(uc,c).numpy(),np.load(f"/home/tiny/weights_cache/ddim/c_in.npy")
-      print(f"| cin | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
-
       latent_uc, latent_c = run(model, fp16r(Tensor.cat(x_t,x_t)), fp16r(Tensor.cat(t_emb,t_emb)), fp16r(Tensor.cat(uc,c))).chunk(2)
       output = latent_uc + cfg_scale * (latent_c - latent_uc)
+
       a,b = output.numpy(), np.load(f"/home/tiny/weights_cache/ddim/model_output_step_{index}.npy")
       print(f"| out | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
+      output = Tensor(b)
 
       shape = (batch_size, 1, 1, 1)
       e_t =   sqrt_alphas_cumprod          .gather(-1, tms).reshape(shape) * output \
             + sqrt_one_minus_alphas_cumprod.gather(-1, tms).reshape(shape) * x_t
-    
-      # a,b = e_t.numpy(),np.load(f"/home/tiny/weights_cache/ddim/e_t_step{index}.npy")
-      # print(f"| e_t | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
+
+      a,b = e_t.numpy(),np.load(f"/home/tiny/weights_cache/ddim/e_t_step{index}.npy")
+      print(f"| e_t | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
 
       a_t               = alphas_cumprod[tms]               .reshape(batch_size,1,1,1)
-      a_prev            = alphas_cumprod_prev[tms]          .reshape(batch_size,1,1,1)
+      a_prev            = alphas_prev[index]                .reshape(batch_size,1,1,1)
       sqrt_one_minus_at = sqrt_one_minus_alphas_cumprod[tms].reshape(batch_size,1,1,1)
+
+      print(f"A_PREV: {a_prev.numpy()}")
 
       pred_x0 =   sqrt_alphas_cumprod          .gather(-1, tms).reshape(shape) * x_t \
                 - sqrt_one_minus_alphas_cumprod.gather(-1, tms).reshape(shape) * output
 
-      # a,b = pred_x0.numpy(),np.load(f"/home/tiny/weights_cache/ddim/pred_x0_step{index}.npy")
-      # print(f"| px0 | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
+      a,b = pred_x0.numpy(),np.load(f"/home/tiny/weights_cache/ddim/pred_x0_step{index}.npy")
+      print(f"| px0 | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
 
       dir_xt = (1.0 - a_prev).sqrt() * e_t
 
-      # a,b = dir_xt.numpy(),np.load(f"/home/tiny/weights_cache/ddim/dir_xt_step{index}.npy")
-      # print(f"| dir | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
+      a,b = dir_xt.numpy(),np.load(f"/home/tiny/weights_cache/ddim/dir_xt_step{index}.npy")
+      print(f"| dir | {np.mean(np.abs(a-b)):.4f} | {np.mean(np.abs(a)):.4f} | {np.mean(np.abs(b)):.4f} |")
 
       x_t = a_prev.sqrt() * pred_x0 + dir_xt
 
