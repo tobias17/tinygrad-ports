3715bfc1c5ff8dab1a06232aa8f4a27a5df98be0
Tobias Fischer <tobiasfischer17@gmail.com>
2024-08-23 23:15:33 +0000

    attempted loading of chunks for sdxl

--- eval_sd/main.py
+++ eval_sd/main.py
@@ -1,7 +1,19 @@
 from tinygrad import Tensor # type: ignore
+from tinygrad.nn.state import load_state_dict, safe_load, get_state_dict
+from examples.sdxl import SDXL, configs # type: ignore
 
 def main():
-  pass
+  MODEL_WEIGHTS_ROOT = "/home/tiny/tinygrad/weights/stable_diffusion_fp16/checkpoint_pipe"
+  model = SDXL(configs["SDXL_Base"])
+
+  sd1 = get_state_dict(model.model.diffusion_model)
+  sd2 = safe_load(f"{MODEL_WEIGHTS_ROOT}/unet/diffusion_pytorch_model.safetensors")
+
+  k1 = sorted(list(sd1.keys()))
+  k2 = sorted(list(sd2.keys()))
+
+  load_state_dict(model.model.diffusion_model, safe_load(f"{MODEL_WEIGHTS_ROOT}/unet/diffusion_pytorch_model.safetensors"))
+  load_state_dict(model.first_stage_model, safe_load(f"{MODEL_WEIGHTS_ROOT}/vae/diffusion_pytorch_model.safetensors"))
 
 if __name__ == "__main__":
   main()
