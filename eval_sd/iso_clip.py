f1dfcdb7301525b40187ab5f3a112a9e419bfcaa
Tobias Fischer <tobiasfischer17@gmail.com>
2024-08-26 18:56:59 -0500

    discovered batched clip issue

--- eval_sd/iso_clip.py
+++ eval_sd/iso_clip.py
@@ -3,14 +3,26 @@ from extra.models.clip import OpenClipEncoder, Tokenizer, clip_configs # type: i
 
 from PIL import Image
 
+SPECIFIC_I = 1
+MAX_INPUTS = 2
+
 def main():
   clip_enc = OpenClipEncoder(**clip_configs["ViT-H-14"]).load_from_pretrained()
   tokenizer = Tokenizer.ClipTokenizer()
 
-  tokens = Tensor(tokenizer.encode("A city at night with people walking around.", pad_with_zeros=True))
-  images = clip_enc.prepare_image(Image.open("inputs/gen_0.png"))
+  with open("inputs/captions.txt", "r") as f:
+    texts = f.read().split("\n")
 
-  score = clip_enc.get_clip_score(tokens.unsqueeze(0).realize(), images.unsqueeze(0).realize())
+  tokens, images = [], []
+  for i, text in enumerate(texts):
+    if SPECIFIC_I >= 0 and i != SPECIFIC_I:
+      continue
+    tokens.append( Tensor(tokenizer.encode(text, pad_with_zeros=True))      .unsqueeze(0) )
+    images.append( clip_enc.prepare_image(Image.open(f"inputs/gen_{i}.png")).unsqueeze(0) )
+    if MAX_INPUTS > 0 and len(tokens) >= MAX_INPUTS:
+      break
+
+  score = clip_enc.get_clip_score(Tensor.cat(*tokens, dim=0).realize(), Tensor.cat(*images, dim=0).realize())
   print(f"score: {score.numpy()}")
 
 if __name__ == "__main__":
