424fd26af9039282ee0695a9b48ed21d63a52f47
Tobias Fischer <tobiasfischer17@gmail.com>
2024-09-19 23:57:13 -0400

    running basic transformer block, tol fixes deltas

--- eval_sd/dual_call_tests.py
+++ eval_sd/dual_call_tests.py
@@ -1,6 +1,6 @@
 from tinygrad import Tensor, dtypes, nn
 from tinygrad.nn.state import get_parameters
-from extra.models.unet import CrossAttention # type: ignore
+from extra.models.unet import CrossAttention, BasicTransformerBlock # type: ignore
 from typing import List, Callable
 import numpy as np
 import unittest
@@ -17,8 +17,8 @@ def make_calls(func:Callable[[Tensor,Tensor],Tensor], b1_i1:Tensor, b2_i1:Tensor
   x1, x2 = func(Tensor.cat(b1_i1, b2_i1), Tensor.cat(b1_i2, b2_i2)).chunk(2)
   y1, y2 = func(b1_i1, b1_i2), func(b2_i1, b2_i2)
 
-  np.testing.assert_allclose(x1.numpy(), y1.numpy())
-  np.testing.assert_allclose(x2.numpy(), y2.numpy())
+  np.testing.assert_allclose(x1.numpy(), y1.numpy(), atol=1e-6, rtol=1e-3)
+  np.testing.assert_allclose(x2.numpy(), y2.numpy(), atol=1e-6, rtol=1e-3)
 
 class Dual_Call_Tests(unittest.TestCase):
   def setUp(self):
@@ -38,6 +38,10 @@ class Dual_Call_Tests(unittest.TestCase):
   def test_self_attention(self):
     ca = randomize_weights(CrossAttention(32, 32, 4, 8))
     make_calls((lambda x,y: ca(x)+ca(y)), *make_input(1, 32, count=4))
+
+  def test_basic_transformer_block(self):
+    btb = randomize_weights(BasicTransformerBlock(32, 64, 4, 8))
+    make_calls(btb, *make_input(1, 4, 32, count=2), *make_input(1, 4, 64, count=2))
 
 if __name__ == "__main__":
   unittest.main()
