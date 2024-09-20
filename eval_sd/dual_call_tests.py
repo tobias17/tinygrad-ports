538e8551a07c935b86d81f45557551e4e39094ff
Tobias Fischer <tobiasfischer17@gmail.com>
2024-09-19 21:04:48 -0400

    got basic deltas

--- eval_sd/dual_call_tests.py
+++ eval_sd/dual_call_tests.py
@@ -1,4 +1,6 @@
 from tinygrad import Tensor
+from tinygrad.nn.state import get_parameters
+from extra.models.unet import CrossAttention # type: ignore
 import numpy as np
 
 def test_1():
@@ -10,5 +12,20 @@ def test_1():
   np.testing.assert_allclose(x1.numpy(), y1.numpy())
   np.testing.assert_allclose(x2.numpy(), y2.numpy())
 
+def test_2():
+  ca = CrossAttention(32, 64, 4, 8)
+  for p in get_parameters(ca):
+    p.replace(Tensor.rand(*p.shape)).realize()
+  
+  a, b = [Tensor.rand(1, 32) for _ in range(2)]
+  c, d = [Tensor.rand(1, 64) for _ in range(2)]
+
+  x1, x2 = ca(Tensor.cat(a, b), Tensor.cat(c, d)).chunk(2)
+  y1, y2 = ca(a, c), ca(b, d)
+
+  np.testing.assert_allclose(x1.numpy(), y1.numpy())
+  np.testing.assert_allclose(x2.numpy(), y2.numpy())
+
 if __name__ == "__main__":
   test_1()
+  test_2()
