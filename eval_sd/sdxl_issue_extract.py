d61a29d25c18993fd9b10fb8fa3092abd7a154e0
Tobias Fischer <tobiasfischer17@gmail.com>
2024-10-06 17:25:03 -0500

    even slimmer

--- eval_sd/sdxl_issue_extract.py
+++ eval_sd/sdxl_issue_extract.py
@@ -10,19 +10,10 @@ def run_not(x, add) -> Tensor:
 if __name__ == "__main__":
   Tensor.manual_seed(0)
 
-  shps = (
-    (1, 4, 32, 32),
-    (1, 4, 32, 32),
-  )
-
   for i in range(5):
-    args1 = [Tensor.randn(shp).realize() for shp in shps]
-    args2 = [Tensor.randn(shp).realize() for shp in shps]
+    a1, a2, a3, a4 = [Tensor.randn(32, 32) for _ in range(4)]
 
-    y_out_1, y_out_2 = run_yes(*args1), run_yes(*args2)
-    n_out_1, n_out_2 = run_not(*args1), run_not(*args2)
-
-    y_comp = (y_out_1 - y_out_2).realize()
-    n_comp = (n_out_1 - n_out_2).realize()
+    y_comp = run_yes(a1, a2) - run_yes(a3, a4)
+    n_comp = run_not(a1, a2) - run_not(a3, a4)
 
     print(f"ITER {i:02d}: delta={Tensor.abs(y_comp - n_comp).mean().numpy():.4f}")
