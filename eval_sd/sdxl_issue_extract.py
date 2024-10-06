9c033b3be79245ee521be0a943f7a4afed1517a0
Tobias Fischer <tobiasfischer17@gmail.com>
2024-10-06 17:16:49 -0500

    refactored computation

--- eval_sd/sdxl_issue_extract.py
+++ eval_sd/sdxl_issue_extract.py
@@ -1,30 +1,29 @@
 from tinygrad import Tensor, TinyJit, dtypes
 
 @TinyJit
-def run_yes(x, c_out, add):
-  return (x*c_out + add).realize()
+def run_yes(x, add):
+  return (x + add).realize()
 
-def run_no(x, c_out, add):
-  return (x*c_out + add).realize()
+def run_not(x, add):
+  return (x + add).realize()
 
 if __name__ == "__main__":
-  Tensor.no_grad = True
   Tensor.manual_seed(0)
 
   shps = (
     (1, 4, 32, 32),
-    (1, 1, 1, 1),
     (1, 4, 32, 32),
   )
 
   for i in range(5):
-    x_pred = {}
-    args1 = [Tensor.randn(shp, dtype=dtypes.float32).realize() for shp in shps]
-    args2 = [Tensor.randn(shp, dtype=dtypes.float32).realize() for shp in shps]
-    for use_jit in [True, False]:
-      def run(*args): return run_yes(*args) if use_jit else run_no(*args)
-      x_u = run(*args1)
-      x_c = run(*args2)
-      x_pred[use_jit] = x_u + 8.0*(x_c - x_u)
+    args1 = [Tensor.randn(shp).realize() for shp in shps]
+    args2 = [Tensor.randn(shp).realize() for shp in shps]
 
-    print(f"ITER {i:02d}: delta={Tensor.abs(x_pred[True] - x_pred[False]).mean().numpy():.4f}")
+    y_out_1, y_out_2 = run_yes(*args1), run_yes(*args2)
+    n_out_1, n_out_2 = run_not(*args1), run_not(*args2)
+
+    def comp(o1, o2) -> Tensor:
+      return o1 + 8.0*(o2 - o1)
+
+    delta = comp(y_out_1, y_out_2) - comp(n_out_1, n_out_2)
+    print(f"ITER {i:02d}: delta={delta.abs().mean().numpy():.4f}")
