c1b863293707f9d9f3e305c827804ee9acefc6a5
Tobias Fischer <tobiasfischer17@gmail.com>
2024-11-19 20:54:43 -0500

    working pytorch generation

--- compare/sdxl_eval_pytorch.py
+++ compare/sdxl_eval_pytorch.py
@@ -1,7 +1,13 @@
 from diffusers import StableDiffusionXLPipeline, EulerDiscreteScheduler
 import torch
+from PIL import Image
+
+GUIDANCE = 8.0
+STEPS = 20
 
 def main():
+  assert torch.cuda.is_available(), "cuda was not available"
+
   model_id = "stabilityai/stable-diffusion-xl-base-1.0"
   scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
   pipe = StableDiffusionXLPipeline.from_pretrained(
@@ -13,40 +19,48 @@ def main():
     torch_dtype=torch.float16,
     device='cuda',
   )
+  pipe.to('cuda')
 
   pos_prompt = "a horse sized cat eating a bagel"
   neg_prompt = ""
 
   with torch.no_grad():
     pos_prompt_embeds_list = []
+    neg_prompt_embeds_list = []
     for tokenizer, text_encoder in zip([pipe.tokenizer, pipe.tokenizer_2], [pipe.text_encoder, pipe.text_encoder_2]):
       pos_text_input = tokenizer(pos_prompt, padding="max_length", max_length=77, truncation=True, return_tensors="pt")
-      text_input_ids = pos_text_input.input_ids
-      pos_prompt_embeds = text_encoder(text_input_ids, output_hidden_states=True)
-      pos_pooled_prompt_embeds = pos_prompt_embeds[0]
-      pos_prompt_embeds_list.append(pos_prompt_embeds)
+      pos_prompt_embeds = text_encoder(pos_text_input.input_ids.cuda(), output_hidden_states=True)
+      pos_prompt_embeds_list.append(pos_prompt_embeds.hidden_states[-2])
+      pos_pooled_embeds = pos_prompt_embeds[0]
 
       neg_text_input = tokenizer(neg_prompt, padding="max_length", max_length=77, truncation=True, return_tensors="pt")
+      neg_prompt_embeds = text_encoder(neg_text_input.input_ids.cuda(), output_hidden_states=True)
+      neg_prompt_embeds_list.append(neg_prompt_embeds.hidden_states[-2])
+      neg_pooled_embeds = pos_prompt_embeds[0]
 
     pos_prompt_embeds = torch.concat(pos_prompt_embeds_list, dim=-1)
+    neg_prompt_embeds = torch.concat(neg_prompt_embeds_list, dim=-1)
 
-    for 
+    print(f"pos_embed_shape: {pos_prompt_embeds.shape}")
+    print(f"neg_embed_shape: {neg_prompt_embeds.shape}")
+    print(f"pos_pooled_shape: {pos_pooled_embeds.shape}")
+    print(f"neg_pooled_shape: {neg_pooled_embeds.shape}")
 
-    prompt_embed = []
-    negative_prompt_embed = []
-    pooled_prompt_embed = []
-    negative_pooled_prompt_embed = []
+    latents_input = torch.randn(1, 4, 128, 128).half().cuda()
 
-    generated = pipe(
+    generated: torch.Tensor = pipe(
       prompt_embeds=pos_prompt_embeds,
-      negative_prompt_embeds=[negative_prompt_embed],
-      pooled_prompt_embeds=[pooled_prompt_embed],
-      negative_pooled_prompt_embeds=[negative_pooled_prompt_embed],
-      guidance_scale=self.guidance,
-      num_inference_steps=self.steps,
+      negative_prompt_embeds=neg_prompt_embeds,
+      pooled_prompt_embeds=pos_pooled_embeds,
+      negative_pooled_prompt_embeds=neg_pooled_embeds,
+      guidance_scale=GUIDANCE,
+      num_inference_steps=STEPS,
       output_type="pt",
       latents=latents_input,
     ).images
+
+    img = Image.fromarray(generated.squeeze(0).permute(1, 2, 0).mul(255.0).to(torch.uint8).detach().cpu().numpy())
+    img.show()
 
 if __name__ == "__main__":
   main()
